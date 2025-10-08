70972611318d0ecedd85165604c60ccca063bbab
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-10-08 19:13:55 +0500

    fix

--- config/celery.py
+++ config/celery.py
@@ -6,7 +6,7 @@ from config.env import env
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", env("DJANGO_SETTINGS_MODULE"))
 
-app = celery.Celery("config")
+app = celery.Celery('config', broker="redis://redis:6379", backend="redis://redis:6379")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
--- config/conf/celery.py
+++ config/conf/celery.py
@@ -1,10 +1,6 @@
 from django.conf import settings
-from config.env import env
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_TIMEZONE = settings.TIME_ZONE
-
-if not env('DEBUG'):
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
